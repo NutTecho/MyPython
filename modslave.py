9d41a148dd142c6c56184787a66c53eae7e3920a
NutTecho <tingting321@windowslive.com>
2021-02-08 19:21:02 +0700

    update modbus

--- modslave.py
+++ modslave.py
@@ -1,18 +1,15 @@
-# from pymodbus.client.sync import ModbusTcpClient
+from pymodbus.client.sync import ModbusTcpClient
 # from pymodbus.client.sync import ModbusSerialClient
 # from pymodbus.server.sync import ModbusTcpServer
 # from pymodbus.diag_message import * 
 # from pymodbus.file_message import *
 # from pymodbus.other_message import *
 # from pymodbus.mei_message import *
-from pymodbus.repl.client import ModbusTcpClient
+# from pymodbus.repl.client import ModbusTcpClient
 # import time
-client = ModbusTcpClient(host='192.168.10.29',port=502)
+# client = ModbusTcpClient(host='192.168.10.29',port=502)
 # a = client.report_slave_id()
 # UNIT = 0x02
-a = client.read_holding_registers(address=1,count= 1,unit = 10)
-client.write_registers(1,[100]*8,unit=2)
-print(a)
 
 # client = ModbusSerialClient(method = 'rtu',port='COM1',baudrate=9600)
 # rr = client.report_slave_id(unit = 1)
@@ -20,15 +17,6 @@ print(a)
 # client.connect()
 # a = client.read_coils(1,1,unit=2).bits[0]
 
-# b1 = client.read_holding_registers(1,1,unit=1).registers[0]
-# rq = r(unit = 1)
-# rr = client.execute(
-# print(rr)
-# time.sleep(1)
-# b2 = client.read_holding_registers(1,1,unit=2).registers[0]
-# print(a)
-# print(b1)
-# print(b2)
 # from easymodbus.modbusClient import ModbusClient
 # client = ModbusClient('127.0.0.1',502)
 # client.unitidentifier = 2
@@ -37,3 +25,35 @@ print(a)
 # client.write_single_register(3,300)
 # client.close()
 # print(a)
+
+
+def WriteData(client,address,count,value):
+    client.write_registers(address,[value]*count,unit=1)
+
+def ReadData(client,address,count):
+    x = client.read_holding_registers(address,count,unit = 1)
+    print(x.registers)
+
+def ReadCoil(client,address,count):
+    x = client.read_coils(address,count,unit=1).bits
+    print(x)
+
+def WriteCoil(client,address,count,value):
+    if value == "ON":
+        status = True
+    else:
+        status = False
+    x = client.write_coils(address,[status]*count,unit=1)
+    # print(x)
+
+
+client = ModbusTcpClient(host='127.0.0.1',port=502)
+print(client.connect())
+# WriteData(client,1,123,5)
+# ReadData(client,1,8)
+# ReadCoil(client,0,8)
+# WriteCoil(client,0,8,"OFF")
+
+
+
+
